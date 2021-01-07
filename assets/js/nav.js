0d53a8f9ee286a4a912f0732085a2e7ffe721ec6
cpvanessa <63360171+cpvanessa@users.noreply.github.com>
2021-01-07 18:51:54 +0100

    Add files via upload

---
+++ assets/js/nav.js
@@ -0,0 +1,10 @@
+function menuburger(){
+    var x = document.getElementById("myTopnav");
+    if(x.className === "topnav"){
+        
+      x.className += " responsive";
+      
+    }else{
+      x.className = "topnav";
+    }
+  }
